51458609abb2879532373a92dcc21c38de5e22c0
Dan Hughes <danhughes.dh+github@gmail.com>
2016-09-19 15:23:08 +0100

    Reduce verbosity in Stories controller

--- app/controllers/Stories.js
+++ app/controllers/Stories.js
@@ -1,16 +1,9 @@
 const stories = require('../models/Story');
 
 module.exports = {
-	index: (req, res) => {
-		return res.render('story/index.html', {
-			stories: stories.all()
-		});
-	},
+	index: (req, res) => res.render('story/index.html', {
+		stories: stories.all()
+	}),
 
-	show: (req, res) => {
-		let story = stories.find(req.params.title);
-		return res.render('story/show.html', {
-			content: story.content
-		});
-	}
+	show: (req, res) => res.render('story/show.html', stories.find(req.params.title))
 };
