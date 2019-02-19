8f87f2180c3357928f10874ea632ffe8b8b42369
Dan Hughes <danhughes@monzo.com>
2019-02-19 18:58:04 +0000

    Remove all non-content

--- app/controllers/Stories.js
+++
@@ -1,9 +0,0 @@
-const stories = require('../models/Story');
-
-module.exports = {
-	index: (req, res) => stories.all()
-		.then(stories => res.render('story/index.html', { stories })),
-
-	show: (req, res) => stories.find(req.params.title)
-		.then(story => res.render('story/show.html', { story }))
-};
--- app/router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const StoryController = require('./controllers/Stories');
-
-module.exports = express.Router()
-	.get('/', StoryController.index)
-	.get('/favicon.ico', (req, res) => res.sendStatus(404))
-	.get('/:title', StoryController.show);
